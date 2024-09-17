f7da398c04e1c68443b01257ee7ede2077fd9ed5
Vinay Joshi <darkerkin4@gmail.com>
2024-09-17 17:57:38 -0400

    Django tutorial completed
    I have finished the django tutorial.

--- ITCS_3135_Django/studybud/studybud/urls.py
+++ ITCS_3135_Django/studybud/studybud/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 
 urlpatterns = [
@@ -24,3 +28,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('base.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
